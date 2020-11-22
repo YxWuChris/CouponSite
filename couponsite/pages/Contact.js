670fb1a7e4b445263ccd9b3c726e8c5fb20e260a
YxWuChris <zy18483@nottingham.edu.cn>
2020-11-21 22:05:34 -0800

    update head of shipping article

--- couponsite/pages/Contact.js
+++ couponsite/pages/Contact.js
@@ -12,6 +12,14 @@ const Contact = () => {
 
     return(
         <>
+        <Head>
+        <title>米国の納税申告書が届くまでにどのくらいかかるの？連邦税と州税の払い戻しに関して詳しく教えてください！</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="アメリカの郵送ガイド USPS・USP・FedEx アメリカから最も安く、早く、信頼できる郵送方法を教えます。アメリカから日本に郵送する方法 アメリカ 郵送方法" />
+        <meta name="keywords" content="ココポンズ,アメリカ 米国 日本 郵送方法 配達 格安 安く 速達 郵送 cocopons.com cocopons" />
+        <meta name="robots" content="index,follow" class="next-head" />
+        <meta name="googlebot" content="index,follow" class="next-head" />
+        </Head>
          <Title />
          <div style={Container}>
          </div>
